f06fc00e1c8892ee7701f4d700c145e0f2c6716e
kassraniroumand <kassra.niroumand@gmail.com>
2025-08-24 12:57:23 +0100

    batman

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Header from './Header.svelte';
+    let name = $state("kasra")
+  let status : 'OPEN' | 'CLOSED' = $state('OPEN')
+  let derivedRes = $derived(name + " " + "niroumand")
+  function onclick() {
+    status = status === "OPEN" ? "CLOSED" : "OPEN"
+
+  }
+</script>
+
+<Header
+name={name}
+full_name={derivedRes}
+/>
+<input type="text" bind:value={name} />
+<!-- <button {onclick}>{status}</button> -->
+<button onclick={() => {
+  status = status === "OPEN" ? "CLOSED" : "OPEN"
+}}>get status</button>
+
+<p>{derivedRes}</p>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let {name, full_name = null}: {name: string, full_name?: string | null} = $props();
+</script>
+
+<div>
+    <h1>Header</h1>
+    <p>Hello {name}</p>
+    <p>{full_name}</p>
+</div>
